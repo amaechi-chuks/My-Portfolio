6d00bf2c60d3d68c2416d73e3574b8a2193443ae
Amaechi Chuks <amaechichuks@Amaechis-MacBook-Air.local>
2019-11-25 22:31:33 +0100

    chore(project setup): setup environment for the application - install devDependencies and dependencies

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+            <p>Hello</p>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+export default App;
